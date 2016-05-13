2c40ad7af5cee25e4a88b85eb9d777edaf1e2d23
CyrilFerlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2016-05-13 11:47:17 +0200

    First page created :)

--- src/Login.java
+++ src/Login.java
@@ -1,15 +1,35 @@
-import javax.servlet.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="annuaire", urlPatterns={"/annuaire"})
+@WebServlet(name = "login", urlPatterns = {"/Login"}, loadOnStartup = 1, initParams = {@WebInitParam(name = "host", value = "localhost"), @WebInitParam(name = "port", value = "8000")})
 public class Login extends HttpServlet {
 
-    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        PrintWriter out = res.getWriter();
-        out.println("Hello, world!");
-        out.close();
+    protected String host;
+    protected Integer port;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        this.host = config.getInitParameter("host");
+        this.port = Integer.valueOf(config.getInitParameter("port"));
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("host", this.host);
+        request.setAttribute("port", this.port);
+        this.getServletContext().getRequestDispatcher("/WebPages/LoginPage.jsp").forward( request, response );
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
---
+++ web/WebPages/LoginPage.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Cyril
+  Date: 13/05/2016
+  Time: 11:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+    <title> Login </title>
+</head>
+<body bgcolor="#f0f0f0">
+    <h1 align="center"> Login </h1>
+    <h2> Host and port: <%= request.getAttribute("host") %> : <%= request.getAttribute("port") %> </h2>
+    <form action="Login" method="POST">
+        <label for="ident">Identifiant:</label>
+        <input id="ident" type="text" name="identifiant">
+        <br/>
+        <label for="pass">Password:</label>
+        <input id= "pass" type="password" name="password"/>
+        <input type="submit" value="Submit"/>
+    </form>
+</body>
+</html>
